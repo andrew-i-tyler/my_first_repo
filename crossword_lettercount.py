3c980bd75e1ce6ccdb429bff92828d937e3cb5e3
andrew-i-tyler <139095496+andrew-i-tyler@users.noreply.github.com>
2023-07-10 00:01:32 -0400

    initial lettercounter code

---
+++ crossword_lettercount.py
@@ -0,0 +1,22 @@
+wordbank = "oncetyleraudreymatthewvowpoetjantwentyseventhwoodstocksqurarefriendsandfamilyschreierstorybooksaintpeteflowercrownphotographlovetuxedocatschristmashappilyeverafterkittenseptemberfirsthalloweenjuneeighteenthwordspumpkinpatchestarget"
+letter_array =[]
+letter_array_totals = []
+for letter in wordbank:
+    if letter not in letter_array: 
+      letter_array.append(letter)
+
+for letter in sorted(letter_array):
+  count = wordbank.count(letter)
+  letter_array_totals.append("{letter} = {count}".format(letter=letter.upper(),count=count))
+
+#for i in letter_array_totals:
+#  print(i)
+
+word_string = ""
+for i in range(0,(len(letter_array_totals))):
+   if letter_array_totals[i] != letter_array_totals[-1]:
+        word_string += str(letter_array_totals[i]) + ", "
+   else:
+      word_string += str(letter_array_totals[i]) + "."
+
+print(word_string)
